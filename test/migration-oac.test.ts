98aed0c9ea0820419b826a2fd64b348706285732
CM-inaba-kanato <inaba.kanato@classmethod.jp>
2023-07-27 16:28:04 +0900

    Initial commit

---
+++ test/migration-oac.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as MigrationOac from '../lib/migration-oac-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/migration-oac-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new MigrationOac.MigrationOacStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
