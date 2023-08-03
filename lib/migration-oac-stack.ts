57ce1c8a565973f7c603b89ed9c51918a1740c54
CM-inaba-kanato <inaba.kanato@classmethod.jp>
2023-08-03 15:18:04 +0900

    [fix] multi origin fixed

--- lib/migration-oac-stack.ts
+++ lib/migration-oac-stack.ts
@@ -7,8 +7,11 @@ import {
   aws_iam,
   aws_s3,
 } from "aws-cdk-lib";
+import { BucketAccessControl } from "aws-cdk-lib/aws-s3";
 import { Construct } from "constructs";
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+// import { BucketDeployment, Source } from "aws-cdk-lib/aws-s3-deployment";
+// import { existsSync } from "fs";
+// import { join } from "path";
 
 export class MigrationOacStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -19,10 +22,25 @@ export class MigrationOacStack extends Stack {
       versioned: true,
       removalPolicy: RemovalPolicy.DESTROY,
       blockPublicAccess: aws_s3.BlockPublicAccess.BLOCK_ALL,
+      // accessControl: BucketAccessControl.PRIVATE,
       cors: [
         {
           allowedMethods: [aws_s3.HttpMethods.GET, aws_s3.HttpMethods.HEAD],
-
+          allowedOrigins: ["*"],
+          allowedHeaders: ["*"],
+        },
+      ],
+    });
+    const mediaBucket = new aws_s3.Bucket(this, "migration-media-bucket", {
+      bucketName: "migration-media-bucket",
+      blockPublicAccess: aws_s3.BlockPublicAccess.BLOCK_ALL,
+      // accessControl: BucketAccessControl.PRIVATE,
+      removalPolicy: RemovalPolicy.DESTROY,
+      versioned: true,
+      cors: [
+        {
+          allowedMethods: [aws_s3.HttpMethods.GET, aws_s3.HttpMethods.HEAD],
+          // Allow specific domain name to retrics others
           allowedOrigins: ["*"],
           allowedHeaders: ["*"],
         },
@@ -50,43 +68,77 @@ export class MigrationOacStack extends Stack {
       defaultBehavior: {
         origin: new aws_cloudfront_origins.S3Origin(appBucket),
       },
+      additionalBehaviors: {
+        "/images/*": {
+          origin: new aws_cloudfront_origins.S3Origin(mediaBucket),
+        },
+      },
       defaultRootObject: "index.html",
     });
 
     const cfnDistribution = distribution.node
       .defaultChild as aws_cloudfront.CfnDistribution;
     // OAI削除（勝手に設定されるため）
-    cfnDistribution.addPropertyOverride(
-      "DistributionConfig.Origins.0.S3OriginConfig.OriginAccessIdentity",
+    cfnDistribution.addOverride(
+      "Properties.DistributionConfig.Origins.0.S3OriginConfig.OriginAccessIdentity",
+      ""
+    );
+    cfnDistribution.addOverride(
+      "Properties.DistributionConfig.Origins.1.S3OriginConfig.OriginAccessIdentity",
       ""
     );
     // OACではCustomOriginConfigは不要
     cfnDistribution.addPropertyDeletionOverride(
       "DistributionConfig.Origins.0.CustomOriginConfig"
     );
+    cfnDistribution.addPropertyDeletionOverride(
+      "DistributionConfig.Origins.1.CustomOriginConfig"
+    );
     // デフォルトではs3のWebsiteURLが設定されてエラーとなるため、S3のドメイン名を設定する
     cfnDistribution.addPropertyOverride(
       "DistributionConfig.Origins.0.DomainName",
       appBucket.bucketRegionalDomainName
     );
+    cfnDistribution.addPropertyOverride(
+      "DistributionConfig.Origins.1.DomainName",
+      mediaBucket.bucketRegionalDomainName
+    );
+
     // OAC設定
     cfnDistribution.addPropertyOverride(
       "DistributionConfig.Origins.0.OriginAccessControlId",
       cfnOriginAccessControl.getAtt("Id")
     );
+    cfnDistribution.addPropertyOverride(
+      "DistributionConfig.Origins.1.OriginAccessControlId",
+      cfnOriginAccessControl.getAtt("Id")
+    );
 
     // S3 - BucketPolicy
-    const bucketPolicyStatement = new aws_iam.PolicyStatement({
+    const appBucketPolicyStatement = new aws_iam.PolicyStatement({
       actions: ["s3:GetObject"],
       effect: aws_iam.Effect.ALLOW,
       principals: [new aws_iam.ServicePrincipal("cloudfront.amazonaws.com")],
       resources: [`${appBucket.bucketArn}/*`],
+      conditions: {
+        StringEquals: {
+          "AWS:SourceArn": `arn:aws:cloudfront::${this.account}:distribution/${distribution.distributionId}`,
+        },
+      },
     });
-    bucketPolicyStatement.addCondition("StringEquals", {
-      "AWS:SourceArn": `arn:aws:cloudfront::${
-        Stack.of(this).account
-      }:distribution/${distribution.distributionId}`,
+    const mediaBucketPolicyStatement = new aws_iam.PolicyStatement({
+      actions: ["s3:GetObject"],
+      effect: aws_iam.Effect.ALLOW,
+      principals: [new aws_iam.ServicePrincipal("cloudfront.amazonaws.com")],
+      resources: [`${mediaBucket.bucketArn}/*`],
+      conditions: {
+        StringEquals: {
+          "AWS:SourceArn": `arn:aws:cloudfront::${this.account}:distribution/${distribution.distributionId}`,
+        },
+      },
     });
-    appBucket.addToResourcePolicy(bucketPolicyStatement);
+
+    appBucket.addToResourcePolicy(appBucketPolicyStatement);
+    mediaBucket.addToResourcePolicy(mediaBucketPolicyStatement);
   }
 }
